d5d14d18429db5d8533eaa1097f95dce5bbb1e51
bjbazzett <60671984+bjbazzett@users.noreply.github.com>
2025-04-28 23:01:53 +0000

    Changed color and orientation of some symbols

--- python/project/sundial.py
+++ python/project/sundial.py
@@ -33,11 +33,13 @@ p_degs = 14
 
 axis_norm_degs = 23.5
 
-day_nums = np.arange(1.5, 366.5, 1)
+day_nums = np.arange(0.5, 366.5, 1)
 
-cal_dict = {1: 'January', 3: 'Perihelion', 32: 'Febuary', 60: 'March', 76: '☀️', 91: 'April', 121: 'May',
-            152: 'June', 171: '☀️', 182: 'July', 185: 'Aphelion', 213: 'August', 244: 'September',
-            266: '☀️', 274: 'October', 305: 'November', 335: 'December', 355: '☀️'}
+cal_dict = {1: 'January', 32: 'Febuary', 60: 'March', 91: 'April', 121: 'May',
+            152: 'June', 182: 'July', 213: 'August', 244: 'September',
+            274: 'October', 305: 'November', 335: 'December'}
+
+cal_dict1 = {3: 'O', 76: '☀️', 171: '☀️', 185: 'O', 266: '☀️', 355: '☀️'}
 
 scaling_on = False
 
@@ -45,7 +47,7 @@ fig = plt.figure(figsize=(10, 6), num='Equation of Time')
 plt.subplots_adjust(top=.925, left=0.100, right=.950, wspace=0.1)
 gs = GridSpec(22, 20, figure=fig)
 
-day_nums = np.arange(1.5, 367.5, 1)  # base calculation on noon UT of day
+day_nums = np.arange(0.5, 366.5, 1)
 min_x, dec_y = eot.analemma_gen(e, p_degs, axis_norm_degs, peri_day, orb_per, day_nums)
 ax_analemma = plt.subplot(gs.new_subplotspec((0, 12), colspan=9, rowspan=22))
 ax_analemma.set_title("Analemma")
@@ -59,29 +61,23 @@ analemma_ann_list = []
 
 for d, dt_lbl in cal_dict.items():
     ann = ax_analemma.annotate(dt_lbl, (min_x[d - 1], dec_y[d - 1]), textcoords="offset points",
-                               xytext=(-30, 0), ha='right', fontsize='small', color='red',
-                               arrowprops=dict(arrowstyle="-", color='red'))
+                               xytext=(-40, 0), ha='center', fontsize='small', color='blue',
+                               arrowprops=dict(arrowstyle="-", color='black'))
+    analemma_ann_list.append(ann)
+
+for d, dt_lbl in cal_dict1.items():
+    ann = ax_analemma.annotate(dt_lbl, (min_x[d - 1], dec_y[d - 1]), textcoords="offset points",
+                               xytext=(0, 20), ha='center', fontsize='small', color='red',
+                               arrowprops=dict(arrowstyle="-", color='black'))
     analemma_ann_list.append(ann)
 
 def update(val):
     global eot_ann_list, analemma_ann_list
 
-    day_nums = np.arange(1.5, 366.5, 1)
-
-
     day_nums = np.arange(0.5, 366.5, 1)
     min_x, dec_y = eot.analemma_gen(e, p_degs, axis_norm_degs, peri_day, orb_per, day_nums)
     analemma_line.set_ydata(dec_y)
     analemma_line.set_xdata(min_x)
-
-    for _, a in enumerate(analemma_ann_list):
-        a.remove()
-    analemma_ann_list[:] = []
-    for d, dt_lbl in cal_dict.items():
-        ann = ax_analemma.annotate(dt_lbl, (min_x[d - 1], dec_y[d - 1]), textcoords="offset points",
-                                   xytext=(-10, 10), ha='right', fontsize='small', color='red',
-                                   arrowprops=dict(arrowstyle="->", color='red'))
-        analemma_ann_list.append(ann)
 
 plt.show()
 
