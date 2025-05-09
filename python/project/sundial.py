77ffe8c25db851f4131d0bb2805287a95354dd1b
bjbazzett <bjbazzett@gmail.com>
2025-05-09 12:49:07 -0500

    naming variables

--- python/project/sundial.py
+++ python/project/sundial.py
@@ -14,29 +14,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 
-e = 0.017
-orb_per = 365.25
+eccentricity = 0.017
+rotation_period = 365.25
 
-peri_day = 5
-p_degs = 14
+perihelion_date = 5
+perihelion_degs = 14
 
 
 axis_norm_degs = 23.5
 
-day_nums = np.arange(1.5, 367.5, 1)
+day = np.arange(1.5, 367.5, 1)
 
-cal_dict = {1: 'January', 32: 'February', 60: 'March', 91: 'April', 182: 'July', 213: 'August'}
+left_months = {1: 'January', 32: 'February', 60: 'March', 91: 'April', 182: 'July', 213: 'August'}
 
-cal_dict1 = {3: '\u2295', 76: '♈', 171: '♊', 185: '\u2295', 266: '♍', 355: '♐'}
+equinoxes_solstices_helions = {3: '\u2295', 76: '♈', 171: '♋︎', 185: '\u2295', 266: '♎︎', 355: '♑︎'}
 
-cal_dict2 = {121: 'May', 152: 'June', 244: 'September', 274: 'October', 305: 'November', 335: 'December'}
+right_months = {121: 'May', 152: 'June', 244: 'September', 274: 'October', 305: 'November', 335: 'December'}
 
 scaling_on = True
 
 fig = plt.figure(figsize=(10, 6), num='Equation of Time')
-gs = GridSpec(22, 20, figure=fig)
+gs = GridSpec(20, 20, figure=fig)
 
-min_x, dec_y = eot.analemma_gen(e, p_degs, axis_norm_degs, peri_day, orb_per, day_nums)
+min_x, dec_y = eot.analemma_gen(eccentricity, perihelion_degs, axis_norm_degs, perihelion_date, rotation_period, day)
 ax_analemma = plt.subplot(gs.new_subplotspec((0, 3), colspan=15, rowspan=22))
 ax_analemma.set_title("Analemma")
 ax_analemma.minorticks_on()
@@ -47,18 +47,18 @@ ax_analemma.set_ylabel('Inches')
 analemma_line, = ax_analemma.plot(min_x, dec_y, 'k', lw=2)
 analemma_ann_list = []
 
-for d, dt_lbl in cal_dict.items():
+for d, dt_lbl in left_months.items():
     ann = ax_analemma.annotate(dt_lbl, (min_x[d - 1], dec_y[d - 1]), textcoords="offset points",
                                xytext=(-155, -2.5), ha='center', fontsize='small', color='blue',
                                arrowprops=dict(arrowstyle="-", color='black'))
     analemma_ann_list.append(ann)
 
-for d, dt_lbl in cal_dict1.items():
+for d, dt_lbl in equinoxes_solstices_helions.items():
     ann = ax_analemma.annotate(dt_lbl, (min_x[d - 1], dec_y[d - 1]), textcoords="offset points",
                                xytext=(0, -10), ha='center', fontsize='30', color='red',
                                arrowprops=dict(arrowstyle="-", color='black'))
     analemma_ann_list.append(ann)
-for d, dt_lbl in cal_dict2.items():
+for d, dt_lbl in right_months.items():
     ann = ax_analemma.annotate(dt_lbl, (min_x[d - 1], dec_y[d - 1]), textcoords="offset points",
                                xytext=(155, -2.5), ha='center', fontsize='small', color='blue',
                                arrowprops=dict(arrowstyle="-", color='black'))
@@ -67,8 +67,8 @@ for d, dt_lbl in cal_dict2.items():
 def update(val):
     global eot_ann_list, analemma_ann_list
 
-    day_nums = np.arange(1.5, 367.5, 1)
-    min_x, dec_y = eot.analemma_gen(e, p_degs, axis_norm_degs, peri_day, orb_per, day_nums)
+    days = np.arange(1.5, 367.5, 1)
+    min_x, dec_y = eot.analemma_gen(eccentricity, day, axis_norm_degs, perihelion_date, rotation_period, days)
     analemma_line.set_ydata(dec_y)
     analemma_line.set_xdata(min_x)
 
