2274a00be9af3b5035b8e674b2d2dc6cb81a41cd
bjbazzett <bjbazzett@gmail.com>
2025-05-14 18:42:24 -0500

    Final work continues

--- python/read_two_columns_text.py
+++ python/read_two_columns_text.py
@@ -6,21 +6,21 @@ __author__ = "ben_bazzett"
 
 import numpy as np
 
-def read_two_column_data(str):
+def read_two_column_data(data):
     try:
-        data = np.loadtxt(str).T
+        data = np.loadtxt(data).T
         if data.shape[0] != 2:
             raise ValueError("File must contain exactly two columns of data.")
         return data
     except OSError as error:
-        raise OSError(f"Could not open file: {str}") from error
+        raise OSError(f"Could not open file: {data}") from error
     except ValueError as error:
-        raise ValueError(f"Invalid file format in: {str}") from error
+        raise ValueError(f"Invalid file format in: {data}") from error
 
 def main():
-    str = 'volumes_energies.dat'
+    data = 'volumes_energies.dat'
     try:
-        data = read_two_column_data(str)
+        data = read_two_column_data(data)
         print(f'{data=}, shape={data.shape}')
     except (OSError, ValueError) as error:
         print(error)
